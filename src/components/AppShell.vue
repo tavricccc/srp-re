2dc74c93439fedf15a23971fee71a7a17a16cc72
tavricccc <177928699+tavricccc@users.noreply.github.com>
2026-07-13 17:04:13 +0800

    Fix sidebar overlap on high zoom

--- src/components/AppShell.vue
+++ src/components/AppShell.vue
@@ -131,7 +131,7 @@
 
     <div
       ref="mainContentRef"
-      class="app-main-content relative mx-auto flex w-full flex-1 flex-col overflow-y-auto overscroll-contain px-4 sm:px-6 lg:px-8"
+      class="app-main-content relative flex flex-1 flex-col overflow-y-auto overscroll-contain px-4 sm:px-6 lg:px-8"
     >
       <main class="min-h-0 flex-1">
         <slot />
